de2018d26389d2b17305f67ae546ef317c93fb5c
JobakLeDev <gautier.guilleron@gmail.com>
2026-05-29 16:44:43 +0200

    Update creation_perso.js

--- pages/creation_perso/creation_perso.js
+++ pages/creation_perso/creation_perso.js
@@ -20,7 +20,11 @@ async function creer(){
     niveau:1, xp:0, hp:10, rad:0, momentum:0, powerArmor:false,
     special:{S:5,P:5,E:5,C:5,I:5,A:5,L:5},
     perks:{}, skills:{en_weapon:0,cac_weapon:0,light_weapon:0,heavy_weapon:0,athletics:0,lockpick:0,speech:0,sneak:0,explosives:0,barehand:0,medicine:0,pilot:0,throwing:0,repair:0,science:0,survival:0,barter:0},
-    taggedSkills:[], inventory:[], ammo:[],
+    taggedSkills:[], inventory:[
+      {name:'Vault 74 Jumpsuit',type:'CLOTHING',qty:1,w:1,equipped:true},
+      {name:'Pipe Gun',type:'WEAPON',qty:1,w:0.9,equipped:true,persoBonus:false},
+      {name:'Stimpak',type:'DRUGS',qty:1,w:0.05,equipped:false},
+    ], ammo:[],
     wounds:{head:false,torso:false,armL:false,armR:false,legL:false,legR:false},
     lastUpdate:Date.now(),
   };
